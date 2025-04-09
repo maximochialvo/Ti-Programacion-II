bc95e175d90a44befbd682781b0e70c50821e06a
maximochialvo <mchialvo@udesa.edu.ar>
2025-04-09 19:59:34 -0300

    haciendo 1.10

--- mercadoLibre/controllers/productoController.js
+++ mercadoLibre/controllers/productoController.js
@@ -1,8 +1,22 @@
+const data = require('../db/datos')
+
+
 const productoController = {
     producto: function (req, res) {
       res.render('productAdd');
+    },
+
+    detalle: function (req, res) {
+      const producto = data.productos[0]
+      res.render('productoDetalle', {producto: producto})
+      
     }
+
+
   };
-  
-  module.exports = productoController;
-  
+
+
+
+
+
+module.exports = productoController;
