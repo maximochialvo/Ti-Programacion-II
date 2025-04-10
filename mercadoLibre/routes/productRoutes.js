d0e8f7c497bd69f1ee2e8bf1fd1466158e32e686
Lukssoncrack <lsilveyrastengel@udesa.edu.ar>
2025-04-10 17:17:58 -0300

    otro

--- mercadoLibre/routes/productRoutes.js
+++ mercadoLibre/routes/productRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const productoController = require('../controllers/productoController');
 
-router.get('/productos', productoController.producto);
+router.get('/', productoController.producto);
 
 module.exports = router;
