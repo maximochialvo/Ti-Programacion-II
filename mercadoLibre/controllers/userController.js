5c9caf2477390bbf63249f3ffa1b60bf17b22320
maximochialvo <mchialvo@udesa.edu.ar>
2025-05-28 17:40:34 -0300

    reparando punto 3

--- mercadoLibre/controllers/userController.js
+++ mercadoLibre/controllers/userController.js
@@ -3,83 +3,81 @@ let db = require("../database/models");
 const bcrypt = require("bcryptjs");
 
 let userController = {
-     index: function (req, res) {
-        if(req.session.user){
-        res.render('profile', { usuario: res.req.user })
-        }else{
+    index: function (req, res) {
+        if (req.session.user) {
+            res.render('profile', { usuario: res.req.user })
+        } else {
             res.redirect('/user/login')
         }
     },
 
-    
+
     show: function (req, res) {
         if (req.session.user != undefined) {
             return res.redirect("/usuario")
-        }else{
-        return res.render("register", {titulo : 'registro'});
+        } else {
+            return res.render("register", { titulo: 'registro' });
         }
 
     },
 
     create: function (req, res) {
 
-        if(req.body.password.length < 3 ){
-    	        return res.send("la contrasenia tiene que tener al menos 3 caracteres")
-            }
-        db.User.findOne({
-            where: [{ email: req.body.email }]
-        })
-        .then(function (usuario) {
-            if (usuario) {
-                return res.send("El usuario ya existe ");
-            }
-
-            const contraseniaValida = bcrypt.hashSync(req.body.password, 10)
-            return db.User.create({
-            name: req.body.usuario,
+        if (req.body.password.length < 3) {
+            return res.send("la contrasenia tiene que tener al menos 3 caracteres")
+        }
+        const contrasenascript = bcrypt.hashSync(req.body.password,10)
+        const user = {
             email: req.body.email,
-            password: contraseniaValida
-        })     
-            .then(function(){
+            contrasena: contrasenascript,
+            usuario: req.body.usuario,
+            birthDate: req.body.birthDate
+        }
+        db.User.create(user)
+
+            .then(function () {
                 return res.redirect('/user/login')
             }
-        )   .catch(function (error) {
+            ).catch(function (error) {
                 return res.send(error)
             })
-      .catch(function (error) {
-            return res.send(error)
-        })
-    })
-            
+
+
+
 
     },
     login: function (req, res) {
-         if (req.session.user != undefined) {
+        if (req.session.user != undefined) {
             return res.redirect("/")
-        }else{
-        return res.render("login", {titulo : 'login'});
-    }},
+        } else {
+            return res.render("login", { titulo: 'login' });
+        }
+    },
 
     processLogin: function (req, res) {
+        console.log('entrando a metodo login')
 
         db.User.findOne({
             where: [{ email: req.body.email }]
         }).then(function (usuario) {
-            if (usuario != null) {
-                const contraseniaValida = bcrypt.compareSync(req.body.password, usuario.password);
+            console.log(usuario)
+            if (usuario) {
+                console.log(req.body.password,usuario.contrasena)
+                const contraseniaValida = bcrypt.compareSync('123456', usuario.contrasena);
                 console.log(contraseniaValida)
                 if (contraseniaValida) {
 
                     req.session.user = usuario
                     if (req.body.recordarme != undefined) {
-                        res.cookie("recordarme", usuario.id, {maxAge: 1000*60*5})
+                        res.cookie("recordarme", usuario.id, { maxAge: 1000 * 60 * 5 })
                     }
                     console.log(req.session.user)
                     return res.redirect("/")
                 } else {
                     return res.send("contraseña no coincide")
-            }}
-            else{
+                }
+            }
+            else {
                 return res.send('Usuario no identificado')
             }
 
@@ -88,15 +86,15 @@ let userController = {
         })
 
 
-       
-  
+
+
     },
 
-    
-    logout: function(req, res) {
-    
+
+    logout: function (req, res) {
+
         req.session.destroy()
-         res.clearCookie("recordarme"); 
+        res.clearCookie("recordarme");
         res.redirect("/");
     }
 
