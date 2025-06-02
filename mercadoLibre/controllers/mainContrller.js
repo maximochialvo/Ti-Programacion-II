ce996ae698729a3f16aec5925b4d8d2b1f9a49ab
Joaquinlanusse <ljoaquin@udesa.edu.ar>
2025-06-02 10:35:12 -0300

    productos del .7

--- mercadoLibre/controllers/mainContrller.js
+++ mercadoLibre/controllers/mainContrller.js
@@ -1,9 +1,15 @@
 const data = require('../db/datos')
+const db = require('../database/models')
+const op = db.Sequelize.Op;
 
 const mainController = {
     index: function(req, res){
-      const productos = data.productos
-      res.render('index', {productos})
+      //const productos = data.productos
+      //res.render('index', {productos})
+      db.Producto.findAll()
+      .then(function(resultados){
+        res.render('index', {productos: resultados})
+      })
     },
 
     buscar: function(req, res) {
